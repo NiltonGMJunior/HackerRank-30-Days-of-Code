4fb5aa11482e5cb3bfa1a8427453a47779b20ce5
Nilton Gomes Martins Junior <nilton.junior51@hotmail.com>
2018-12-25 11:34:30 -0200

    Days 15-17 and partial day 18 submission

--- Day15/linked_list.py
+++ Day15/linked_list.py
@@ -5,19 +5,37 @@ class Node:
 
 
 class Solution:
-    def display(self, ArithmeticErrorhead):
+    def display(self, head):
         current = head
         while current:
             print(current.data, end=' ')
             current = current.next
 
     def insert(self, head, data):
-        # TODO: Implement insert method
+        new_node = Node(data)
+        if head:
+            current = head
+            while current.next:
+                current = current.next
+            current.next = new_node
+            return head
+        else:
+            return new_node
+
+
+# mylist = Solution()
+# T = int(input())
+# head = None
+# for i in range(T):
+#     data = int(input())
+#     head = mylist.insert(head, data)
+# mylist.display(head)
 
 mylist = Solution()
-T = int(input())
+T = int("4")
 head = None
+inp = ["2", "3", "4", "1"]
 for i in range(T):
-    data = int(input())
+    data = int(inp[i])
     head = mylist.insert(head, data)
 mylist.display(head)
